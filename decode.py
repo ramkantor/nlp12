3d2b7a6ac2c1f32bd03777c4ce7d54a38d4b3042
ramkantor <35624537+ramkantor@users.noreply.github.com>
2018-04-26 03:32:20 +0300

    Update decode.py

--- decode.py
+++ decode.py
@@ -9,6 +9,12 @@ def run(model, test_file, param_file_1, param_file_2):
         '1': src.BasicTagger.decode,
         '2': src.HmmDecoder.decode
     }
+"""
+"run" is a little ambiguous. What if you had 2 run functions from different modules (imported as the functions and not the modules)
+Something more understandable than '1', '2'.
+You can have a "methods" dictionary in a separate file. This way it won't be computed every time you apply "run", even though
+it's done only once. But the idea of a model(/methods) dict' is correct, and would have fit even more if there were more than 2 models.
+"""
 
     methods[model](test_file, param_file_1, param_file_2)
 
@@ -20,4 +26,7 @@ if __name__ == "__main__":
     param_file_2=None
     if len(sys.argv) > 4:
         param_file_2 = sys.argv[4]
+"""
+param_file_2 = sys.argv[4] if len(sys.argv) > 4 else None
+"""
     run(model, test_file, param_file_1, param_file_2)
