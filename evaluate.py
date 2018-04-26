17da55d81d579c86299b850e369ad7fd0cbd6cbf
ramkantor <35624537+ramkantor@users.noreply.github.com>
2018-04-26 03:43:22 +0300

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -9,6 +9,12 @@ def run(tagged_file, gold_file, model, smoothing):
     print model
     names = {'1': 'basic-', '2': 'hmm-smoothing-'}
     src.accuracy.eval(tagged_file, gold_file, model, smoothing, names[model] + smoothing)
+"""
+Can also be: 
+src.accuracy.eval(tagged_file, gold_file, model, smoothing, ('basic-' if model == '1' else 'hmm-smoothing-') + smoothing)
+You can check for validy of script args before, this is the correct way. If you won't and ise my suggestion then if the input is '3' then
+it's not sure what the user wants
+"""
 
 
 if __name__ == "__main__":
